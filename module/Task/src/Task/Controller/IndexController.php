695a6f4fa09a78de57ed1ecfb97fec8b114d4d14
folevo <dima_teteruk@mail.ru>
2015-02-03 00:29:17 +0200

    dimas

--- module/Task/src/Task/Controller/IndexController.php
+++ module/Task/src/Task/Controller/IndexController.php
@@ -8,13 +8,12 @@ use \PDO;
 use Zend\View\Model\JsonModel;
 class IndexController extends AbstractActionController
 {
-    //вывод грида
     public function indexAction()
     {
 
         return new ViewModel();
     }
-//возвращение json
+
     public function dataAction()
     {
 
@@ -30,10 +29,10 @@ class IndexController extends AbstractActionController
 
         $request = $this->getRequest();
 
-        //получение параметров пагинации
+        //Получаем данные
         $start = $request->getPost('start') ? : 0;
         $limit = $request->getPost('limit') ? : 5;
-//получение параметров сорттировки
+
 
         if($sort=$request->getPost('sort')){
             $sort=Json::decode(rtrim(ltrim($request->getPost('sort'),'['),']'),Json::TYPE_ARRAY);
@@ -41,19 +40,25 @@ class IndexController extends AbstractActionController
         }else{
             $sort='';
         }
-        //задание количества записей на выборку
         $offset = ' LIMIT '.$start.','.$limit;
 
         $stmt = $adapter->createStatement('SELECT  name,view_education,city FROM userview '.$sort." ".$offset);
         $results = $stmt->execute();
+       $count=$adapter->createStatement('SELECT  COUNT(*) as total FROM userview')->execute();
+        foreach($count as $ct){
+            $total=$ct;
+        }
+
+
 
         $data="[";
         foreach($results as $result){
             $data.=Json::encode($result).",";
 
         }
-        //получение json
+
         $data=substr($data, 0,-1);
+        $data.=",".Json::encode($total);
         $data.="]";
         $result = new JsonModel(array(
             'data' => $data,
@@ -62,9 +67,8 @@ class IndexController extends AbstractActionController
 
         return $result;
     }
-
     public function updateAction(){
-//подключение к бд
+
         $adapter = new Adapter( array(
                 'driver' => 'Pdo',
                 'dsn' => 'mysql:dbname=task;host=localhost',
@@ -74,7 +78,6 @@ class IndexController extends AbstractActionController
 
             )
         );
-        //обновление данных
         $sql = "UPDATE education
 
         SET  view_education=?
