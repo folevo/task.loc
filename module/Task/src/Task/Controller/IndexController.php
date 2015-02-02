aa412e46f7e455762f04f628591a3485a407e8fe
folevo <dima_teteruk@mail.ru>
2015-02-02 11:15:34 +0200

    ddd

--- module/Task/src/Task/Controller/IndexController.php
+++ module/Task/src/Task/Controller/IndexController.php
@@ -8,12 +8,13 @@ use \PDO;
 use Zend\View\Model\JsonModel;
 class IndexController extends AbstractActionController
 {
+    //вывод грида
     public function indexAction()
     {
 
         return new ViewModel();
     }
-
+//возвращение json
     public function dataAction()
     {
 
@@ -29,10 +30,10 @@ class IndexController extends AbstractActionController
 
         $request = $this->getRequest();
 
-        //Получаем данные
+        //получение параметров пагинации
         $start = $request->getPost('start') ? : 0;
         $limit = $request->getPost('limit') ? : 5;
-
+//получение параметров сорттировки
 
         if($sort=$request->getPost('sort')){
             $sort=Json::decode(rtrim(ltrim($request->getPost('sort'),'['),']'),Json::TYPE_ARRAY);
@@ -40,6 +41,7 @@ class IndexController extends AbstractActionController
         }else{
             $sort='';
         }
+        //задание количества записей на выборку
         $offset = ' LIMIT '.$start.','.$limit;
 
         $stmt = $adapter->createStatement('SELECT  name,view_education,city FROM userview '.$sort." ".$offset);
@@ -50,6 +52,7 @@ class IndexController extends AbstractActionController
             $data.=Json::encode($result).",";
 
         }
+        //получение json
         $data=substr($data, 0,-1);
         $data.="]";
         $result = new JsonModel(array(
@@ -59,8 +62,9 @@ class IndexController extends AbstractActionController
 
         return $result;
     }
-    public function updateAction(){
 
+    public function updateAction(){
+//подключение к бд
         $adapter = new Adapter( array(
                 'driver' => 'Pdo',
                 'dsn' => 'mysql:dbname=task;host=localhost',
@@ -70,6 +74,7 @@ class IndexController extends AbstractActionController
 
             )
         );
+        //обновление данных
         $sql = "UPDATE education
 
         SET  view_education=?
